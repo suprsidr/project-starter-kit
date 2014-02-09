5e61f9f346b82b5e717f618eddcd0fc9b3b13e59
suprsidr <suprsidr@gmail.com>
2014-02-09 10:46:07 -0600

    Initial

---
+++ Gruntfile.js
@@ -0,0 +1,132 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    
+    pngmin: {
+      compile: {
+        options: {
+          binary: 'bin/pngquant.exe', // specify pngquant path
+          concurrency: 8,             // specify how many exucutables get spawned in parallel
+          colors: 128,                // reduce colors to 128
+          ext: '.png',                // use .png as extension for the optimized files default: -opt.png
+          quality: '30-60',           // output quality should be between 65 and 80 like jpeg quality
+          speed: 10,                  // pngquant should be as fast as possible
+          force: false                // force overwrites if destination file exists - very expensive    
+        },
+        files: [
+          {
+            expand: true,
+            src: ['**/*.png'],
+            cwd: 'img-src/',
+            dest: 'img/'
+          }
+        ]
+      }
+    },
+    
+    jpgmin: {
+      compile: {
+        options: {
+          ext: '.jpg',                // use .jpg as extension for the optimized files default: -opt.jpg
+          progressive: true,          // progressive jpg output
+          force: false                // force overwrites if destination file exists - very expensive    
+        },
+        files: [
+          {
+            expand: true,
+            src: ['**/*.{jpg,JPG}'],
+            cwd: 'img-src/',
+            dest: 'img/'
+          }
+        ]
+      }
+    },
+    
+    resizes: {
+      compile: {
+        options: {
+          force: false,                // force overwrites if destination file exists - very expensive
+          quality: 0.8,                // output quality
+          progressive: true,           // progressive jpg
+          strip: true,                 // strip metadata
+          sizes: [250, 450, 650]       // array of resizes. images will be named image-250.jpg, image-450.jpg, image-650.jpg...
+        },
+        files: [
+          {
+            expand: true,
+            src: ['**/*.{jpg,JPG,png}'],
+            cwd: 'resize/',
+            dest: 'img/'
+          }
+        ]
+      }
+    },
+    
+    uglify: {
+      dist: {
+        options: {
+          outputStyle: 'compressed'
+        },
+        files: {
+          'js/main.min.js': 'js/main.js'
+        }
+      }
+    },
+
+    sass: {
+      options: {
+        includePaths: ['css/scss/foundation']
+      },
+      dev: {
+        files: {
+          'css/main.css': 'css/scss/main.scss'
+        }        
+      },
+      dist: {
+        options: {
+          outputStyle: 'compressed'
+        },
+        files: {
+          'css/main.min.css': 'css/scss/main.scss'
+        }        
+      }
+    },
+
+    watch: {
+      grunt: { files: ['Gruntfile.js'] },
+      
+      uglify: {
+        files: 'js/main.js',
+        tasks: ['uglify']
+      },
+      
+      sass: {
+        files: 'css/scss/*.scss',
+        tasks: ['sass']
+      },
+      
+      pngmin: {
+        files: 'img-src/**/*.png',
+        tasks: ['pngmin']
+      },
+      
+      jpgmin: {
+        files: 'img-src/**/*.{jpg,JPG}',
+        tasks: ['jpgmin']
+      },
+      
+      resizes: {
+        files: 'resize/**/*.{jpg,JPG,png}',
+        tasks: ['resizes']
+      }
+    }
+  });
+  
+  grunt.loadNpmTasks('grunt-image-processor');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  
+  grunt.registerTask('build', ['sass', 'uglify', 'pngmin', 'jpgmin', 'resizes']);
+  grunt.registerTask('default', ['build','watch']);
+}
---
+++ README.md
@@ -0,0 +1,38 @@
+# Horizon project template
+
+This is a template to start your own project that uses Grunt and libsass!
+
+## Requirements
+
+You'll need to have the following items installed before continuing.
+
+  * [Ruby](http://rubyinstaller.org/): Install the 1.9.x branch
+  * [Node.js](http://nodejs.org): Use the installer provided on the NodeJS website.
+  * [Grunt](http://gruntjs.com/): Run `[sudo] npm install -g grunt-cli`
+  * [Bower](http://bower.io): Run `[sudo] npm install -g bower`
+  * [ImageMagick](http://www.imagemagick.org/script/binary-releases.php#windows): Install Win64 dynamic at 16 bits-per-pixel v.6.8.x or better
+
+## Quickstart
+
+Make sure you have bower and grunt-cli installed globally:
+
+```bash
+npm install -g bower grunt-cli
+```
+
+Then install project dependencies locally:
+
+```bash
+npm install
+```
+
+While you're working on your project, run:
+
+`grunt`
+
+And you're set!
+
+## Directory Strucutre
+
+  * `css/scss/_settings.scss`: Foundation configuration settings go in here
+  * `css/scss/main.scss`: Application styles go here
